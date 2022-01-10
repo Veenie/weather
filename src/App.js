46a53dcf8b729072ecc5caac23757edccead71c6
Veenie <raveen08@gmail.com>
2022-01-10 13:11:06 -0800

    css adjust, corresponding divs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <main>
+        <div className='location-box'>
+          <div className='location'></div>
+          <div className='date'></div>
+          
+        </div>
+      </main>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
